424d651df91ba85a00cab751414b026be0a3612b
AustinC02 <77993889+AustinC02@users.noreply.github.com>
2025-01-11 13:56:01 -0600

    Update __init__.py

--- subsystems/__init__.py
+++ subsystems/__init__.py
@@ -1,7 +1,10 @@
 # Imports
 from .SampleSubsystem import *
-
+from .Climber import *
+from .Elevator import *
 # Classes to Import
 __all__ = [
-    "SampleSubsystem"
+    "SampleSubsystem",
+    "Climber",
+    "Elevator",
 ]
